ff1384688e1fd3c703c0e484eb37a7ebd55383cd
dasoya <66422476+dasoya@users.noreply.github.com>
2024-02-22 23:44:09 +0900

    add longer curve

---
+++ LogerCurveMainView.swift
@@ -0,0 +1,18 @@
+//
+//  LogerCurveMainView.swift
+//  2024challenge
+//
+//  Created by dasoya on 2/22/24.
+//
+
+import SwiftUI
+
+struct LogerCurveMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogerCurveMainView()
+}
