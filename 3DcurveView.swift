6f18fa7f08ada2d6a32e2fd03eb64049505b6bff
dasoya <66422476+dasoya@users.noreply.github.com>
2024-02-25 14:18:27 +0900

    add 3d but mix

---
+++ 3DcurveView.swift
@@ -0,0 +1,18 @@
+//
+//  3DcurveView.swift
+//  2024challenge
+//
+//  Created by dasoya on 2/25/24.
+//
+
+import SwiftUI
+
+struct _DcurveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _DcurveView()
+}
